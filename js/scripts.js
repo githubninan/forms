98fc5fbb29bd89ae77c96de0c1384fab7fb0d4ea
Nina Nikoghosyan <ninain@gmail.com>
2019-02-14 15:00:33 -0800

    create forms: letter, text to uppercase, order receipt

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
+$("#blanks form").submit(function(event) {
 var person1Input = $("input#inputName").val();
 $(".person1").text(person1Input);
 $("#blanks").hide();
 $("#letter").fadeIn();
+event.preventDefault();
+  });
+
+  $("button#click-submit").click(function() {
+
+$("textarea#textInput").addClass("uppercaseText");
+});
+
+$("#order form").submit(function(event) {
+var nameInput = $("input#fullName").val();
+var addressInput = $("input#address").val();
+var cityInput = $("input#city").val();
+var zipcodeInput = $("input#zipcode").val();
+
+$(".customer").text(orderInput);
+$("div#order").hide();
+$("div#receipt").fadeIn();
 event.preventDefault();
   });
 });
